aa13be7efc41c52227cdd5e8cfe75add418792f0
Will Brandin <will.brandin@humminghomes.com>
2023-09-04 16:21:47 -0600

    public init for core data client

--- Sources/CoreDataClient/Client.swift
+++ Sources/CoreDataClient/Client.swift
@@ -5,4 +5,14 @@ public struct CoreDataClient {
     public var container: NSPersistentContainer
     public var viewContext: NSManagedObjectContext
     public var newBackgroundContext: NSManagedObjectContext
+
+    public init(
+        container: NSPersistentContainer,
+        viewContext: NSManagedObjectContext,
+        newBackgroundContext: NSManagedObjectContext
+    ) {
+        self.container = container
+        self.viewContext = viewContext
+        self.newBackgroundContext = newBackgroundContext
+    }
 }
