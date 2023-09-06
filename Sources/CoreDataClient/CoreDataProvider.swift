a689dcaee470255027ba63ae3894d1f8cffc8fa7
Will Brandin <william.brandin@gmail.com>
2023-09-05 22:05:27 -0600

    add structured logging

--- Sources/CoreDataClient/CoreDataProvider.swift
+++ Sources/CoreDataClient/CoreDataProvider.swift
@@ -1,6 +1,12 @@
 import CoreData
 import Dependencies
 import Foundation
+import os.log
+
+extension Logger {
+    private static var subsystem = Bundle.main.bundleIdentifier!
+    static let coreData = Logger(subsystem: subsystem, category: "core_data_stack")
+}
 
 public class CoreDataProvider {
     public private(set) var container: NSPersistentCloudKitContainer
@@ -37,10 +43,16 @@ public class CoreDataProvider {
         #endif
     }
 
-    internal static func setupCloudKitContainer(container name: String, model: NSManagedObjectModel, withSync: Bool, inMemory: Bool = false) -> NSPersistentCloudKitContainer {
+    internal static func setupCloudKitContainer(
+        container name: String,
+        model: NSManagedObjectModel,
+        withSync: Bool,
+        inMemory: Bool = false
+    ) -> NSPersistentCloudKitContainer {
         let container = NSPersistentCloudKitContainer(name: name, managedObjectModel: model)
 
         if inMemory {
+            Logger.coreData.debug("Initialized in Memory")
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
 
@@ -48,26 +60,36 @@ public class CoreDataProvider {
             fatalError("###\(#function): Failed to retrieve a persistent store description.")
         }
 
-        print("cloudkit container identifier : \(description.cloudKitContainerOptions?.containerIdentifier ?? "")")
+        Logger.coreData.debug("CloudKit container details: \(description, privacy: .sensitive)")
 
-        description.setOption(true as NSNumber,
-                              forKey: NSPersistentHistoryTrackingKey)
+        description.setOption(
+            true as NSNumber,
+            forKey: NSPersistentHistoryTrackingKey
+        )
 
-        description.setOption(true as NSNumber,
-                              forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
+        description.setOption(
+            true as NSNumber,
+            forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey
+        )
 
         if !withSync {
+            Logger.coreData.debug("Disabling iCloud Sync")
             description.cloudKitContainerOptions = nil
         }
 
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
+            } else {
+                Logger.coreData.debug("loadPersistentStores did succeed")
             }
         })
 
+        Logger.coreData.debug("Setting viewContext.automaticallyMergesChangesFromParent")
         container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
 
+        Logger.coreData.debug("Did return container")
         return container
     }
 }
