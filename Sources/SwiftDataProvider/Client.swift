1b93e2f708b02544589c5f2291a344a71325648f
Will Brandin <will.brandin@humminghomes.com>
2023-09-10 21:15:25 -0600

    Update Client.swift

--- Sources/SwiftDataProvider/Client.swift
+++ Sources/SwiftDataProvider/Client.swift
@@ -3,10 +3,37 @@ import SwiftData
 
 @available(iOS 17, *)
 public struct SwiftDataClient {
-    public var modelContext: () -> ModelContext
+    public var container: () -> ModelContainer
 }
 
+@available(iOS 17, *)
 public class SwiftDataProvider {
-    
+    var modelContainer: ModelContainer
+
+    public init(
+        schema: Schema,
+        isStoredInMemoryOnly: Bool,
+        appGroupId: String? = nil
+    ) {
+        var groupContainer: ModelConfiguration.GroupContainer!
+        if let appGroupId {
+            groupContainer = .identifier(appGroupId)
+        } else {
+            groupContainer = .automatic
+        }
+
+        let modelConfiguration = ModelConfiguration(
+            schema: schema,
+            isStoredInMemoryOnly: isStoredInMemoryOnly,
+            groupContainer: groupContainer,
+            cloudKitDatabase: .automatic
+        )
+
+        do {
+            self.modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
 }
 #endif
