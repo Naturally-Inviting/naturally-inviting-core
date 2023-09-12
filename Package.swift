724a52d6caadbe91c42729272f5ad8c92fb4e737
Will Brandin <will.brandin@humminghomes.com>
2023-09-12 09:10:36 -0600

    update to swift 5.9 and ios 17 minimum

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "naturally-inviting-core",
     platforms: [
-        .iOS(.v16),
-        .watchOS(.v9),
+        .iOS(.v17),
+        .watchOS(.v10),
         .macOS(.v13)
     ],
     products: [
