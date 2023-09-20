755d0ea44622bc747a5817d4d2e61bbda1e365de
Will Brandin <will.brandin@humminghomes.com>
2023-09-20 13:26:57 -0600

    Update Interface.swift

--- Sources/UserDefaultsClient/Interface.swift
+++ Sources/UserDefaultsClient/Interface.swift
@@ -18,4 +18,26 @@ public struct UserDefaultsClient {
     public var setData: @Sendable (Data?, String) async -> Void
     public var setDouble: @Sendable (Double, String) async -> Void
     public var setInteger: @Sendable (Int, String) async -> Void
+
+    public init(
+        boolForKey: @escaping @Sendable (String) -> Bool,
+        dataForKey: @escaping @Sendable (String) -> Data?,
+        doubleForKey: @escaping @Sendable (String) -> Double,
+        integerForKey: @escaping @Sendable (String) -> Int,
+        remove: @escaping @Sendable (String) async -> Void, 
+        setBool: @escaping @Sendable (Bool, String) async -> Void,
+        setData: @escaping @Sendable (Data?, String) async -> Void, 
+        setDouble: @escaping @Sendable (Double, String) async -> Void,
+        setInteger: @escaping @Sendable (Int, String) async -> Void
+    ) {
+        self.boolForKey = boolForKey
+        self.dataForKey = dataForKey
+        self.doubleForKey = doubleForKey
+        self.integerForKey = integerForKey
+        self.remove = remove
+        self.setBool = setBool
+        self.setData = setData
+        self.setDouble = setDouble
+        self.setInteger = setInteger
+    }
 }
