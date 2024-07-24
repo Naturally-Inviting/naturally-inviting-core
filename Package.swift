2039d3ae78c90244c26d19b0de26abb0bbb55546
Will Brandin <william.brandin@gmail.com>
2024-07-23 22:10:07 -0600

    update

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/Naturally-Inviting/swift-tca-custom-alert",
-            from: "0.3.1"
+            from: "0.3.2"
         )
     ],
     targets: [
